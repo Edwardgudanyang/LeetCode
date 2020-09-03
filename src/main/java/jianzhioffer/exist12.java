3b1896a02455be9b89273a933b17cd3a3b48bd67
Edward <danyang.gu@hand-china.com>
2020-09-03 15:32:49 +0800

    剑指offer  exist 12

---
+++ src/main/java/jianzhioffer/exist12.java
@@ -0,0 +1,30 @@
+package main.java.jianzhioffer;
+
+public class exist12 {
+    public boolean exist(char[][] board, String word) {
+        if (word.length() == 0) {
+            return true;
+        }
+        char[] words = word.toCharArray();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (deepSearch(board, words, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean deepSearch(char[][] board, char[] word, int i, int j, int k) {
+        if(i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != word[k]) return false;
+        if(k == word.length - 1 )  return true;
+        char tmp = board[i][j];
+        board[i][j] = '/';
+        boolean res = deepSearch(board, word, i + 1, j, k + 1) || deepSearch(board, word, i - 1, j, k + 1) ||
+                deepSearch(board, word, i, j + 1, k + 1) || deepSearch(board, word, i , j - 1, k + 1);
+        board[i][j] = tmp;
+        return res;
+
+    }
+}
