51c60a316af27c6c82070b2c3625c6563514664f
gudanyang <danyang.gu@hand-china.com>
2020-10-21 16:45:50 +0800

    985,714

--- src/main/java/solution985/sumEvenAfterQueries.java
+++ src/main/java/solution985/sumEvenAfterQueries.java
@@ -29,4 +29,25 @@ public class sumEvenAfterQueries {
         }
         return res;
     }
+//    public int[] sumEvenAfterQueries(int[] A, int[][] queries) {
+//        int n=A.length;
+//        int[] res=new int[n];
+//        int s=0;
+//        for(int j=0;j<n;j++){
+//            if(A[j]%2==0){
+//                s+=A[j];
+//            }
+//        }
+//        for(int i=0;i<n;i++){
+//            if(A[queries[i][1]]%2==0){
+//                s-=A[queries[i][1]];
+//            }
+//            A[queries[i][1]]+=queries[i][0];
+//            if(A[queries[i][1]]%2==0){
+//                s+=A[queries[i][1]];
+//            }
+//            res[i]=s;
+//        }
+//        return res;
+//    }
 }
